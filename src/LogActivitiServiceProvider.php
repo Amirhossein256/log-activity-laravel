815bb18e79031f70634785f3b53cc078e548f7f4
amirhossein <a.najarbashi@greenweb.ir>
2022-03-26 10:24:23 +0430

    initial commit

---
+++ src/LogActivitiServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+
+use Illuminate\Support\ServiceProvider;
+
+class LogActivitiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
